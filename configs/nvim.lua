89fa7d44b7c1ddcc1c0ce0303f9fd496020ec0f4
Nicolas Bach <nicolas@mia3.de>
2025-12-26 23:38:33 +0100

    clean up the file

--- configs/nvim.lua
+++ configs/nvim.lua
@@ -1,21 +1,34 @@
+-------------------------------------
+-- ############################### --
+-- ############################### --
+----------- Neovim Config -----------
+-- ############################### --
+-- ############################### --
+-------------------------------------
 
--- Bootstrap lazy.nvim
+---------------------------
+--- Bootstrap lazy.nvim ---
+---------------------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({"git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath})
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.vim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+local out = vim.fn.system({"git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath})
+if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+        { "Failed to clone lazy.vim:\n", "ErrorMsg" },
+        { out, "WarningMsg" },
+        { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+end
 end
 vim.opt.rtp:prepend(lazypath)
 
+-----------------------
+--- Set vim options ---
+-----------------------
+--- maybe some are unnecessary
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.opt.number = true                 -- show line numbers
@@ -30,31 +43,51 @@ vim.opt.encoding = "utf-8"            -- set encoding to utf-8
 vim.opt.termguicolors = true          -- set to false to get opacity
 vim.opt.mouse = "a"
 
+---------------------
+--- Setup Plugins ---
+---------------------
+-- Plugin List:
+-- folke/tokyonight.nvim                # colorscheme
+-- kylechui/nvim-surround               # surround motions
+-- neovim/nvim-lspconfig                # lspconfig for autocompletion
+-- nvim-lualine/lualine.nvim            # lualine as an equivalent for airline
+-- nvim-tree/nvim-tree.lua              # filebrowser
+-- nvim-tree/nvim-web-devicons          # nerdfonts and icons for nvim-tree
+-- nvim-treesitter/nvim-treesitter      # Treesitter
+-- nvim-telescope/telescope.nvim        # fuzzy finding
+-- saghen/blink.cmp                     # lsp client
+-- windwp/nvim-autopairs                # auto pairs
 require("lazy").setup({
     spec = {
         {
-            "windwp/nvim-autopairs", -- Autopairs
+            "windwp/nvim-autopairs", 
             event = "InsertEnter",
             config = true
         },
         {
-            "folke/tokyonight.nvim", -- Theme from lazy
+            "kylechui/nvim-surround",
+            event = "VeryLazy",
+            config = function()
+                require("nvim-surround").setup({})
+            end
+        },
+        {
+            "folke/tokyonight.nvim",
             lazy = false,
             priority = 1000,
             opts = {}
         },
         {
-            "nvim-lualine/lualine.nvim", -- lualine, nicer line at the bottom
+            "nvim-lualine/lualine.nvim",
             dependencies = { "nvim-tree/nvim-web-devicons"}
         },
-        "nvim-tree/nvim-tree.lua", -- file browser
-        "nvim-tree/nvim-web-devicons", -- icons via nerd font for file browser
+        "nvim-tree/nvim-tree.lua", 
+        "nvim-tree/nvim-web-devicons", 
         {
             "nvim-treesitter/nvim-treesitter",
             lazy = false,
             build = ":TSUpdate"
         },
-        -- "nvim-lua/plenary.nvim",
         {
             "nvim-telescope/telescope.nvim", tag = "v0.2.0",
             dependencies = { "nvim-lua/plenary.nvim" }
@@ -64,7 +97,7 @@ require("lazy").setup({
             build = "cargo +nightly build --release",
             dependencies = { "rafamadriz/friendly-snippets" },
             opts = {
-                keymap = { 
+                keymap = {
                     preset = "default",
                     ["<Tab>"] = { "select_next", "fallback"},
                     ["<S-Tab>"] = {"select_prev", "fallback"},
@@ -82,19 +115,20 @@ require("lazy").setup({
     },
     install = { colorscheme = { "tokyonight" } },
 })
+
 -----------------------
 --- NVIM Tree Setup ---
 -----------------------
 require("nvim-tree").setup({
-    sort = {
-        sorter = "case_sensitive",
-    },
-    view = {
-        width = 30,
-    },
-    filters = {
-        dotfiles = true,
-    },
+sort = {
+    sorter = "case_sensitive",
+},
+view = {
+    width = 30,
+},
+filters = {
+    dotfiles = true,
+},
 })
 
 ---------------------
@@ -102,9 +136,8 @@ require("nvim-tree").setup({
 ---------------------
 require('lualine').get_config()
 require('lualine').setup {
-	options = { theme = 'tokyonight' },
+options = { theme = 'tokyonight' },
 }
-
 
 -----------------
 --- LSP Setup ---
@@ -114,24 +147,24 @@ local capabilities = require('blink.cmp').get_lsp_capabilities()
 
 -- Rust Autocompletion
 vim.lsp.config('rust_analyzer', {
-    capabilities = capabilities
+capabilities = capabilities
 })
 vim.lsp.enable('rust_analyzer')
 
 -- Python Auto Completion
 vim.lsp.config('pyright', {
-    capabilities = capabilities
+capabilities = capabilities
 })
 vim.lsp.enable('pyright')
 
 -- Necessary for error messages
 vim.diagnostic.config({
-    virtual_text = true,
-    signs = true,
-    update_in_insert = false,
-    underline = true,
-    severity_sort = true,
-    float = { border = "rounded" },
+virtual_text = true,
+signs = true,
+update_in_insert = false,
+underline = true,
+severity_sort = true,
+float = { border = "rounded" },
 })
 
 -----------------
@@ -147,9 +180,12 @@ vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help ta
 ---------------------
 --- Customization ---
 ---------------------
-vim.api.nvim_set_keymap('n', '<F2>', ':NvimTreeToggle<CR>', {noremap= true})
-vim.api.nvim_set_keymap('n', '<F3>', ':bo term<CR>', {noremap= true})
-vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', {noremap= true})
-vim.api.nvim_set_keymap('n', '<leader>p', ':pc<CR>', {noremap= true})
-vim.api.nvim_set_keymap("n", "<leader>n", ":noh<CR>", { desc = "execute noh to remove highlighting"})
 vim.cmd[[colorscheme tokyonight]]
+vim.api.nvim_set_keymap("n", "<leader>k", ":NvimTreeToggle<CR>", { desc = "toggle neovim"})
+vim.api.nvim_set_keymap("n", "<leader>t", ":bo term<CR>i", { desc = "open terminal in new Buffer"})
+vim.api.nvim_set_keymap("n", "<leader>w", ":w<CR>", { desc = "save file without quitting"})
+vim.api.nvim_set_keymap("n", "<leader>p", ":pc<CR>", { desc = "get help out of the way"}) -- maybe YCM specific and not necessary anymore
+vim.api.nvim_set_keymap("n", "<leader>n", ":noh<CR>", { desc = "execute noh to remove highlighting"})
+vim.api.nvim_set_keymap("n", "<leader>W", [[/\s\+$<CR>]], { desc = "Show trailing whitespaces"})
+vim.api.nvim_set_keymap("t", "<Esc>", [[<C-\><C-n>]], { desc = "map esc in terminal mode"})
+
